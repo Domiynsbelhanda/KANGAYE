b3d1fd01c60f37c9d626e315020b4d029d943c48
Dominique Youness <youness.dominique@gmail.com>
2020-02-21 15:50:06 +0200

    odification

--- app/src/main/java/cd/belhanda/kangaye/MainActivity.java
+++ app/src/main/java/cd/belhanda/kangaye/MainActivity.java
@@ -1,18 +1,23 @@
 package cd.belhanda.kangaye;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import android.view.MenuItem;
 import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -21,10 +26,23 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
+import cd.belhanda.kangaye.Modele.Inscription_Modele;
+import cd.belhanda.kangaye.ui.SOS.SOSFragment;
+import cd.belhanda.kangaye.ui.alerte.AlerteFragment;
+import cd.belhanda.kangaye.ui.contact.ContactFragment;
+import cd.belhanda.kangaye.ui.home.HomeFragment;
+import cd.belhanda.kangaye.ui.profil.ProfilFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DatabaseReference mDatabase;
+    String key, nom, prenom, pseudo, telephone, mail;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,29 +50,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        final RelativeLayout fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        );
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        
+
+        displaySelectedScreen(R.id.nav_home);
+
+        /** Firebase get key start*/
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                fab.setVisibility(View.GONE);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    Inscription_Modele connexion = dataSnapshot1.getValue(Inscription_Modele.class);
+
+                    if (connexion.getPseudo().equals("Dominique")){
+                        key = dataSnapshot1.getKey();
+                        nom = connexion.getNom();
+                        prenom = connexion.getPrenom();
+                        pseudo = connexion.getPseudo();
+                        telephone = connexion.getTelephone();
+                        mail = connexion.getMail();
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
-
-
-
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        /** FIrebase get key end*/
     }
 
     @Override
@@ -65,9 +102,72 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        displaySelectedScreen(menuItem.getItemId());
+
+        return true;
+    }
+
+    public void displaySelectedScreen(int itemId){
+
+        Fragment fragment = null;
+
+        switch (itemId){
+            case R.id.nav_home:
+                Bundle bundle1 = new Bundle();
+                bundle1.putString("key", key);
+                bundle1.putString("nom", nom);
+                bundle1.putString("prenom", prenom);
+                bundle1.putString("pseudo", pseudo);
+                bundle1.putString("telephone", telephone);
+                bundle1.putString("mail", mail);
+                fragment = new HomeFragment();
+                fragment.setArguments(bundle1);
+                break;
+            case R.id.nav_profil:
+                Bundle bundle = new Bundle();
+                bundle.putString("key", key);
+                bundle.putString("nom", nom);
+                bundle.putString("prenom", prenom);
+                bundle.putString("pseudo", pseudo);
+                bundle.putString("telephone", telephone);
+                bundle.putString("mail", mail);
+                fragment = new ProfilFragment();
+                fragment.setArguments(bundle);
+
+                break;
+            case R.id.nav_contact:
+                Bundle bundles = new Bundle();
+                bundles.putString("key", key);
+                bundles.putString("nom", nom);
+                bundles.putString("prenom", prenom);
+                bundles.putString("pseudo", pseudo);
+                bundles.putString("telephone", telephone);
+                bundles.putString("mail", mail);
+                fragment = new ContactFragment();
+                fragment.setArguments(bundles);
+                break;
+            case R.id.nav_alert:
+                fragment = new AlerteFragment();
+                break;
+            case R.id.nav_sos:
+                fragment = new SOSFragment();
+                break;
+            case R.id.nav_carte:
+                startActivity(new Intent(this, MapsActivity.class));
+                break;
+        }
+
+        if(fragment != null && itemId != R.id.nav_carte){
+
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        } else{
+            startActivity(new Intent(this, MapsActivity.class));
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
     }
 }
--- app/src/main/java/cd/belhanda/kangaye/ui/home/HomeFragment.java
+++ app/src/main/java/cd/belhanda/kangaye/ui/home/HomeFragment.java
@@ -4,32 +4,137 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.UUID;
+
+import cd.belhanda.kangaye.Modele.Inscription_Modele;
 import cd.belhanda.kangaye.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+    CardView SOS, ALERTE;
+    Button btnSendAlertes;
+    EditText editSendAlertes;
+    private String key, nom, prenom, pseudo, telephone, mail;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
+
+        DatabaseReference mDatabase;
+
+        /** Firebase get key start*/
+        mDatabase = FirebaseDatabase.getInstance().getReference("Users");
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    Inscription_Modele connexion = dataSnapshot1.getValue(Inscription_Modele.class);
+
+                    if (connexion.getPseudo().equals("Dominique")){
+                        key = dataSnapshot1.getKey();
+                        nom = connexion.getNom();
+                        prenom = connexion.getPrenom();
+                        pseudo = connexion.getPseudo();
+                        telephone = connexion.getTelephone();
+                        mail = connexion.getMail();
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        /** FIrebase get key end*/
+
+        final RelativeLayout fab = root.findViewById(R.id.fab);
+        final RelativeLayout alerte = root.findViewById(R.id.alerte_bottom);
+        final RelativeLayout sender = root.findViewById(R.id.sendAlerte);
+
+        btnSendAlertes = root.findViewById(R.id.btnSendAlertes);
+        editSendAlertes = root.findViewById(R.id.editSendAlertes);
+        editSendAlertes.setHint(pseudo);
+
+        sender.setVisibility(View.GONE);
+        alerte.setVisibility(View.GONE);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fab.setVisibility(View.GONE);
+                alerte.setVisibility(View.VISIBLE);
+            }
+        });
+
+        SOS = root.findViewById(R.id.SOSLANCEUR);
+        ALERTE = root.findViewById(R.id.ALERTELANCEUR);
+
+        ALERTE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alerte.setVisibility(View.GONE);
+                sender.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        btnSendAlertes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String dates = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+                String heures = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date());
+
+                DatabaseReference databaseReference;
+                databaseReference= FirebaseDatabase.getInstance().getReference("Alertes");
+                String key = databaseReference.push().getKey();
+
+                databaseReference = databaseReference.child(key);
+
+                databaseReference.child("pseudo").setValue(pseudo);
+                databaseReference.child("nom_alerte").setValue(editSendAlertes.getText().toString());
+                databaseReference.child("Localisation").setValue("");
+                databaseReference.child("Date").setValue(dates);
+                databaseReference.child("Heure").setValue(heures);
+            }
+        });
+
+
         return root;
     }
+
 }
